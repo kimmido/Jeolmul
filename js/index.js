b6b99054de19c0edbf50e8d5ba47521ab39fde76
kimmido <icecream120102@gmail.com>
2023-01-16 14:39:32 +0900

    사진추가, js 완

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
 $(function() {
-    const gnbFix = $('header').height();
-    console.log(gnbFix);
-
-    // $("#title").on("click", function() {
-    //     $('html').animate({'scrollTo' : 0}, 2000);
-    // });
-
-    $(window).on("scroll", function() {
-        // if(gnbFix < $(window).scrollTop()) {
-        //     $(".title-loginmenu-mainmenu").addClass("fixed");
-        // } else {
-        //     $(".title-loginmenu-mainmenu").removeClass("fixed");
-        // }
+    $('.main-menu').on({
+        "mouseenter" : function() {
+            $(".title-loginmenu-mainmenu").addClass("header-hover");
+            $('.lnb').fadeIn(500, "linear");
+        },
+        "mouseleave" : function() {
+            $(".title-loginmenu-mainmenu").removeClass("header-hover");
+            $('.lnb').fadeOut(200, "linear");
+        }
     })
+
+
+
+    const bannerDot = $('.img-nav > span');
+
+    bannerDot.on("click", function() {
+        let index = bannerDot.index(this);
+        bannerDot.removeClass("on");
+        $(this).addClass("on")
+
+        $('header').fadeOut(300, "linear", function() {
+            $('header').css({"background-image" : `url('../images/main-img(${index}).jpg')` })
+            $('header').fadeIn(300, "linear")
+        })
+    })
+
+
 
     $('#minus').on("click", function() { guestUpDown(-1) });
     $('#plus').on("click", function() { guestUpDown(1) });
+
     $('#guestNum').on("change", function() { 
         let num = Number($('#guestNum').val());
         limit(num);
@@ -32,6 +46,37 @@ $(function() {
         if(num > 20 ) { num = 20; }
         $('#guestNum').val(num);
     }
+    
+    
+
+    $('.lt-bt').on("click", function() { galleryChange(-1) })
+    $('.gt-bt').on("click", function() { galleryChange(1) })
+    
+    const galleryImg = $('.gallery-img');
+    const galleryBar = $('.gallery-bar span');
+    const galleryTxt = $('.gallery-txt li');
+    let num = 0;
+
+    function galleryChange(plusMinus) {
+        num += plusMinus;
+        num = galleryLimit(num);
+
+        galleryImg.attr("src", `./images/gallery${ num + 1}.png`)
+        galleryImg.attr("alt", `절물자연휴양림 풍경${ num + 1}`)
+        galleryBar.removeClass("on");
+        galleryBar.eq(num).addClass("on");
+        galleryTxt.removeClass("on");
+        galleryTxt.eq(num).addClass("on");
+    }
+    
+    function galleryLimit(num) {
+        if(num < 0 ) { num = galleryBar.length - 1; }
+        if(num >= galleryBar.length ) { num = 0; }
+        
+        return num;
+    }
+
+    
 
     const flexWidth = $('.room-list-flex').width();
     const scrollBt = $('.scroll-bt').width();
@@ -41,12 +86,20 @@ $(function() {
     console.log(scrollBt);
     console.log(scrollWidth);
 
-    $('.scroll-bt').on("click", function(e) { moveBt(e) });
+    $('.scroll-bt').on("mousedown", function(e) { 
+        $(this).on("mousemove", function(e) {
+            moveBt(e) 
+        })
+    });
 
     function moveBt(e) {
         // e.target.position().left
-        console.log($(e.target).position().top);
-        console.log(this);
-        console.log($(this));
+        console.log($(e.target).position().left);
     }
+
+
+
+
+
+
 }); 
